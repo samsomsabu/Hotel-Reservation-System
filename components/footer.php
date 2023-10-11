c2930112ee4f3b25fd8d19813f32896cce44fad1
SAMSON SABU <139948132+samsomsabu@users.noreply.github.com>
2023-10-11 15:53:25 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +123-456-7890</a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i> +111-222-3333</a>
+         <a href="mailto:ridhinabraham@outlook.com"><i class="fas fa-envelope"></i> ridhinabraham@outlook.com</a>
+         <a href="mailto:samsonsabu@gmail.com"><i class="fas fa-envelope"></i> samsonsabu@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Bangalore,Karnataka, india - 575001</a>
+      </div>
+
+      <div class="box">
+         <a href="#home">home</a>
+         <a href="#about">about</a>
+         <a href="bookings.php">my bookings</a>
+         <a href="#reservation">reservation</a>
+         <a href="#gallery">gallery</a>
+         <a href="#contact">contact</a>
+         <a href="#reviews">reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="https://www.facebook.com/">Facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="https://twitter.com/">Twitter <i class="fab fa-twitter"></i></a>
+         <a href="https://www.instagram.com/">Instagram <i class="fab fa-instagram"></i></a>
+         <a href="https://www.linkedin.com/">linkedin <i class="fab fa-linkedin"></i></a>
+         <a href="https://www.youtube.com/">Youtube <i class="fab fa-youtube"></i></a>
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; Prisitine Hotel Group Pvt. Ltd</div>
+
+</section>
+
+<!-- footer section ends -->
